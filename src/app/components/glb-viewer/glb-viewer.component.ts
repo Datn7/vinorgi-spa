dad41313dbbb34cc346aad8dd7d48b8903803a37
Datn7 <datunashvili72@gmail.com>
2025-06-27 01:29:14 +0400

    glb viewer and threejs

---
+++ src/app/components/glb-viewer/glb-viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-glb-viewer',
+  imports: [],
+  templateUrl: './glb-viewer.component.html',
+  styleUrl: './glb-viewer.component.scss'
+})
+export class GlbViewerComponent {
+
+}
