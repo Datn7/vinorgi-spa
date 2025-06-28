d31c2d620057ffc1789b23e490dc67934726c241
datunashviliWeinor <gdatunashvili@weinor.de>
2025-06-28 21:10:42 +0400

    added landing hero component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingHeroComponent } from "./components/landing-hero/landing-hero.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, LandingHeroComponent],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss',
