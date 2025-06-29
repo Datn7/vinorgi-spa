9507ba1963e8cbd46d27df1ab21bbbe29daa689f
Datn7 <datunashvili72@gmail.com>
2025-06-29 21:16:55 +0400

    minor bugfix removed hero component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LandingHeroComponent } from "./components/landing-hero/landing-hero.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, LandingHeroComponent],
+  imports: [RouterOutlet, NavbarComponent],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss',
