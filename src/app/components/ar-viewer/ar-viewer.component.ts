53edaac2af74b608d6f7ebaeb59a2f9fb3f5da07
Datn7 <datunashvili72@gmail.com>
2025-07-17 18:17:44 +0400

    made a-frame working

--- src/app/components/ar-viewer/ar-viewer.component.ts
+++ src/app/components/ar-viewer/ar-viewer.component.ts
@@ -4,56 +4,18 @@ import {
   CUSTOM_ELEMENTS_SCHEMA,
   OnInit,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ar-viewer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './ar-viewer.component.html',
   styleUrls: ['./ar-viewer.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ArViewerComponent implements OnInit {
-  modelUrl: string | null = null;
-  isBrowser = false;
-
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private cdRef: ChangeDetectorRef
-  ) {}
-
-  ngOnInit(): void {
-    this.isBrowser = typeof window !== 'undefined';
-
-    const modelId = this.route.snapshot.queryParamMap.get('modelId');
-    if (modelId) {
-      this.http
-        .get<any>(`http://localhost:5151/api/models/${modelId}`)
-        .subscribe({
-          next: (data) => {
-            const base = window.location.origin;
-            this.modelUrl = base + data.fileUrl;
-
-            // Trigger change detection manually to avoid ExpressionChanged error
-            this.cdRef.detectChanges();
-          },
-          error: () => {
-            console.error('Model failed to load.');
-          },
-        });
-    }
-  }
-
-  enterAR(): void {
-    const viewer: any = document.querySelector('model-viewer');
-    if (viewer && viewer.activateAR) {
-      viewer.activateAR();
-    } else {
-      alert('AR mode not supported on this device.');
-    }
-  }
+export class ArViewerComponent {
+  constructor(private http: HttpClient) {}
 }
