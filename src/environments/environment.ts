fb3e5b6c810576d4807e0ec27ae386a1fb3e7127
Datn7 <datunashvili72@gmail.com>
2025-07-17 15:00:57 +0400

    updated url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7088/api', // change to your actual dev API base
+  apiUrl: 'http://localhost:5151/api', // change to your actual dev API base
 };
